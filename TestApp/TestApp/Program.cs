20d33e7c648ac687feda3d309e2077a4ecb0cbd0
MartaRomaniv <55060324+MartaRomaniv@users.noreply.github.com>
2019-09-27 01:21:42 +0300

    Add files via upload

--- TestApp/TestApp/Program.cs
+++ TestApp/TestApp/Program.cs
@@ -33,5 +33,65 @@ namespace TestApp
             }
             Console.WriteLine();
         }
+
+        static void ShowUsers(ComfortDevTestContext db)
+        {
+            Console.WriteLine("Users:");
+            foreach (User user in db.User)
+            {
+                Console.WriteLine($"Name: {user.Name}\t| Hash: {user.Hash}");
+            }
+            Console.WriteLine();
+        }
+
+        static void ShowTasks(ComfortDevTestContext db)
+        {
+            Console.WriteLine("Tasks:");
+            foreach (Task task in db.Task)
+            {
+                Console.WriteLine($"Id: {task.Id}\t| Title: {task.Title}\t| TopicID: {task.TopicId}\t| EvalCrit: {task.EvalCrit}");
+            }
+            Console.WriteLine();
+        }
+
+        static void ShowCourseTasks(ComfortDevTestContext db)
+        {
+            Console.WriteLine("Course tasks:");
+            foreach (CourseTask courseTask in db.CourseTask)
+            {
+                Console.WriteLine($"Id: {courseTask.Id}\t| CourseId: {courseTask.CourseId}\t| TaskID: {courseTask.TaskId}\t| CompPer: {courseTask.CompPer}");
+            }
+            Console.WriteLine();
+        }
+
+        static void ShowTestAnswers(ComfortDevTestContext db)
+        {
+            Console.WriteLine("Test Answers:");
+            foreach (TestAnswer testAnswer in db.TestAnswer)
+            {
+                Console.WriteLine($"Id: {testAnswer.Id}\t| QuestionId: {testAnswer.QuestionId}\t| Answer: {testAnswer.Answer}\t| TopicId: {testAnswer.TopicId}");
+            }
+            Console.WriteLine();
+        }
+
+        static void ShowTestQuestions(ComfortDevTestContext db)
+        {
+            Console.WriteLine("Test Question:");
+            foreach (TestQuestion testQuestion in db.TestQuestion)
+            {
+                Console.WriteLine($"Id: {testQuestion.Id}\t| Question: {testQuestion.Question}");
+            }
+            Console.WriteLine();
+        }
+
+        static void ShowUserCourses(ComfortDevTestContext db)
+        {
+            Console.WriteLine("User Courses:");
+            foreach (UserCourse userCourse in db.UserCourse)
+            {
+                Console.WriteLine($"Id: {userCourse.Id}\t| UserName: {userCourse.UserName}\t| EndDate: {userCourse.EndDate}");
+            }
+            Console.WriteLine();
+        }
     }
 }
