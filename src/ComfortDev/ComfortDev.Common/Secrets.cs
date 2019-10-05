b873a48d57d2c4e59aa25e3f866c43ba07865591
Maksym Dovhan <kerman901@gmail.com>
2019-10-06 00:39:26 +0300

    change jwt key

--- src/ComfortDev/ComfortDev.Common/Secrets.cs
+++ src/ComfortDev/ComfortDev.Common/Secrets.cs
@@ -6,6 +6,6 @@ namespace ComfortDev.Common
     {
         public static string ConnectionString { get; } = "Host=localhost;Port=5432;Database=ComfortDev;Username=postgres;Password=1058";
         public static string MD5Key { get; } = "JaNdRgUjXn2r5u8x/A?D(G+KbPeShVmYp3s6v9y$B&E)H@McQfTjWnZr4t7w!z%C";
-        public static string JwtKey { get; } = "9y$B&E)H@McQfTjW";
+        public static string JwtKey { get; } = "6w9z$C&E)H@McQfTjWnZr4u7x!A%D*G-JaNdRgUkXp2s5v8y/B?E(H+MbPeShVmY";
     }
 }
