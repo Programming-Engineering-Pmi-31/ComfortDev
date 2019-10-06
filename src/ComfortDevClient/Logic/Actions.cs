9d8241c5356ada8cd80c015c44cd2f494f7cf68e
Maksym Dovhan <kerman901@gmail.com>
2019-10-07 02:22:48 +0300

    add ComfortDevClient project

---
+++ src/ComfortDevClient/Logic/Actions.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace Logic
+{
+    public static class Actions
+    {
+        private const string API_ADDRESS = "http://localhost:31415/api";
+        private static string token = null;
+
+        public static string RegisterUser(string username, string password)
+        {
+            var pairs = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("username", username),
+                new KeyValuePair<string, string>("Password", password)
+            };
+            var content = new FormUrlEncodedContent(pairs);
+
+            using (var client = new HttpClient())
+            {
+                var response = client.PostAsync(API_ADDRESS + "/user/register", content).Result;
+                return response.StatusCode.ToString();
+            }
+        }
+
+        public static void AuthenticateUser(string username, string password)
+        {
+            var pairs = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("username", username),
+                new KeyValuePair<string, string>("Password", password)
+            };
+            var content = new FormUrlEncodedContent(pairs);
+
+            using (var client = new HttpClient())
+            {
+                var response = client.PostAsync(API_ADDRESS + "/user/authenticate", content).Result;
+                var result = response.Content.ReadAsStringAsync().Result;
+                token = JsonConvert.DeserializeObject<Dictionary<string, string>>(result)["token"];
+            }
+        }
+    }
+}
