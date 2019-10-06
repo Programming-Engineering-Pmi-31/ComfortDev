34983214a2ddcdbb3253be266160eeed93ef1176
Igor Payenskii <38349979+Lvivsky@users.noreply.github.com>
2019-10-06 20:54:07 +0300

    Registration form

--- LoginScreen/5/tems.xaml.cs
+++ LoginScreen/5/tems.xaml.cs
@@ -10,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace _5
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for tems.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class tems : Window
     {
-        public MainWindow()
+        public tems()
         {
             InitializeComponent();
-        }
-
-        private void textBox2_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
         }
     }
 }
