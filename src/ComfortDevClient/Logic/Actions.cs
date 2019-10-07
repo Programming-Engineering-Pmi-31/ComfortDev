6c1a447bc5635f5100e039fd5a590ee36f38c59b
Maksym Dovhan <kerman901@gmail.com>
2019-10-07 04:30:21 +0300

    add register method to client add authentication method to client

--- src/ComfortDevClient/Logic/Actions.cs
+++ src/ComfortDevClient/Logic/Actions.cs
@@ -2,42 +2,34 @@
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
-
+using Flurl.Http;
 namespace Logic
 {
     public static class Actions
     {
-        private const string API_ADDRESS = "http://localhost:31415/api";
+        private const string API_ADDRESS = "http://localhost:31415";
         private static string token = null;
 
         public static string RegisterUser(string username, string password)
         {
-            var pairs = new List<KeyValuePair<string, string>>
-            {
-                new KeyValuePair<string, string>("username", username),
-                new KeyValuePair<string, string>("Password", password)
-            };
-            var content = new FormUrlEncodedContent(pairs);
-
             using (var client = new HttpClient())
             {
-                var response = client.PostAsync(API_ADDRESS + "/user/register", content).Result;
-                return response.StatusCode.ToString();
+                var response = client.PostAsync(
+                    API_ADDRESS + $"/api/user/register/?username={username}&password={password}", 
+                    null
+                );
+                return response.Result.StatusCode.ToString();
             }
         }
 
         public static void AuthenticateUser(string username, string password)
         {
-            var pairs = new List<KeyValuePair<string, string>>
-            {
-                new KeyValuePair<string, string>("username", username),
-                new KeyValuePair<string, string>("Password", password)
-            };
-            var content = new FormUrlEncodedContent(pairs);
-
             using (var client = new HttpClient())
             {
-                var response = client.PostAsync(API_ADDRESS + "/user/authenticate", content).Result;
+                var response = client.PostAsync(
+                    API_ADDRESS + $"/api/user/authenticate/?username={username}&password={password}", 
+                    null
+                ).Result;
                 var result = response.Content.ReadAsStringAsync().Result;
                 token = JsonConvert.DeserializeObject<Dictionary<string, string>>(result)["token"];
             }
