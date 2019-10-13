d9de19c74c497ac92c06b68a4035fcf6605ace20
Maksym Dovhan <kerman901@gmail.com>
2019-10-14 01:25:52 +0300

    improve client actions

--- src/ComfortDevClient/Logic/Actions.cs
+++ src/ComfortDevClient/Logic/Actions.cs
@@ -1,16 +1,23 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 
 namespace Logic
 {
+    public struct ActionResult
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public string Message { get; set; }
+    }
+
     public static class Actions
     {
         private const string API_ADDRESS = "http://localhost:31415";
         private static string token = null;
 
-        public static string RegisterUser(string username, string password)
+        public static ActionResult RegisterUser(string username, string password)
         {
             using (var client = new HttpClient())
             {
@@ -18,21 +25,42 @@ namespace Logic
                     API_ADDRESS + $"/api/user/register/?username={username}&password={password}", 
                     null
                 );
-                return response.Result.StatusCode.ToString();
+
+                return new ActionResult
+                {
+                    StatusCode = response.Result.StatusCode,
+                    Message = GetDataDict(response.Result)["message"]
+                };
             }
         }
 
-        public static void AuthenticateUser(string username, string password)
+        public static ActionResult AuthenticateUser(string username, string password)
         {
             using (var client = new HttpClient())
             {
                 var response = client.PostAsync(
-                    API_ADDRESS + $"/api/user/authenticate/?username={username}&password={password}", 
+                    API_ADDRESS + $"/api/user/authenticate/?username={username}&password={password}",
                     null
-                ).Result;
-                var result = response.Content.ReadAsStringAsync().Result;
-                token = JsonConvert.DeserializeObject<Dictionary<string, string>>(result)["token"];
+                );
+
+                var dataDict = GetDataDict(response.Result);
+                if (response.Result.StatusCode == HttpStatusCode.OK)
+                {
+                    token = dataDict["token"];
+                }
+
+                return new ActionResult
+                {
+                    StatusCode = response.Result.StatusCode,
+                    Message = dataDict["message"]
+                };
             }
+        }
+
+        static Dictionary<string, string> GetDataDict(HttpResponseMessage httpResponse)
+        {
+            string rawData = httpResponse.Content.ReadAsStringAsync().Result;
+            return JsonConvert.DeserializeObject<Dictionary<string, string>>(rawData);
         }
     }
 }
