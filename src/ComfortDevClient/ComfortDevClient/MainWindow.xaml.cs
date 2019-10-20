b66397621096b85c6f1ad9c9806943c41e5e385c
Igor Payenskii <38349979+Lvivsky@users.noreply.github.com>
2019-10-20 18:03:16 +0300

    Connect buttons to BD

--- src/ComfortDevClient/ComfortDevClient/MainWindow.xaml.cs
+++ src/ComfortDevClient/ComfortDevClient/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.util;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,12 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Logic;
 
-namespace ComfortDevClient
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+namespace ComfortDevClient {
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -25,16 +23,27 @@ namespace ComfortDevClient
             InitializeComponent();
         }
 
-        private void button_register_Click(object sender, RoutedEventArgs e)
-        {
-            Hide();
-            RegisterWindow w = new RegisterWindow();
-            w.Show();
-        }
-
         private void button_login_Click(object sender, RoutedEventArgs e)
         {
+            if (loginBox.Text.Length < 4 || passwordBox.Password.Length <= 8)
+                MessageBox.Show("Your login or password is wrong!");
+            else
+            {
+                MessageBox.Show("Welcome");
+                Actions.AuthenticateUser(loginBox.Text, passwordBox.Password);
+            }
+                
+        }
 
+        private void button_regist_Click(object sender, RoutedEventArgs e)
+        {
+            if (loginBox.Text.Length < 4 || passwordBox.Password.Length <= 8)
+                MessageBox.Show("Your login or password is short, please try again!");
+            else 
+            {
+                MessageBox.Show("Register successful");
+                Actions.RegisterUser(loginBox.Text, passwordBox.Password);
+            }
         }
     }
 }
