1c6d12de0ad1a3f53ab1e1a0aa241518b5b07590
Maksym Dovhan <kerman901@gmail.com>
2019-11-10 17:33:56 +0200

    Add UserCoursesServise, UserCoursesController

---
+++ src/ComfortDev/ComfortDev.API/Controllers/UserCoursesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ComfortDev.BLL.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComfortDev.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserCoursesController : ControllerBase
+    {
+        private readonly UserCoursesService userService;
+
+        public UserCoursesController()
+        {
+            userService = new UserCoursesService();
+        }
+
+        [HttpPost("create")]
+        public IActionResult CreateCourse(int topicId)
+        {
+            try
+            {
+                int userId = int.Parse(User.Identity.Name);
+                var createdCourse = userService.CreateCourse(userId, topicId, 3, 5);
+                return Ok(createdCourse);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+    }
+}
