baa053ad0d83e9edb948641a39eef59eb5d64788
Maksym Dovhan <kerman901@gmail.com>
2019-11-10 17:32:56 +0200

    Add JsonIgnore attributes

--- src/ComfortDev/ComfortDev.Common/Entities/Task.cs
+++ src/ComfortDev/ComfortDev.Common/Entities/Task.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace ComfortDev.Common.Entities
 {
@@ -12,11 +13,15 @@ namespace ComfortDev.Common.Entities
 
         public int Id { get; set; }
         public string Title { get; set; }
+
+        [JsonIgnore]
         public int TopicId { get; set; }
         public string TaskText { get; set; }
         public string EvalCrit { get; set; }
 
         public virtual Topic Topic { get; set; }
+
+        [JsonIgnore]
         public virtual ICollection<CourseTask> CourseTasks { get; set; }
     }
 }
